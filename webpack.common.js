321803dec1f5aa1b759056d43b9b4bcafd424e8c
Ondrej Cifka <cifkao@users.noreply.github.com>
2020-07-28 18:30:06 +0200

    Get rid of tabs

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ const config = {
   entry: {
     common: __dirname + '/_js/common.ts',
     demo: __dirname + '/_js/demo.ts',
-		interpolation: __dirname + '/_js/interpolation.js'
+    interpolation: __dirname + '/_js/interpolation.js'
   },
   output:{
     path: __dirname + '/assets/js',
