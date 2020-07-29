12e765b8c6cc064d018a65e7363196d594f199dc
Ondrej Cifka <cifkao@users.noreply.github.com>
2020-07-29 10:00:38 +0200

    Cleaner build and deployment

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
-#!/bin/sh
-rsync -avzH --delete --exclude .htaccess _site/ groove2groove:www/
+#!/bin/bash
+set -ex
+
+yarn run clean
+yarn run build
+rsync -avzH --delete --exclude .htaccess _site/* groove2groove:www/
+rsync -rvH --delete --existing --ignore-existing --exclude .htaccess _site/ groove2groove:www/
