e5459cd4c4bafd838d6cb73a0fb37147b59f18b0
Ondrej Cifka <cifkao@users.noreply.github.com>
2021-04-27 18:41:58 +0200

    Add checkpoints, disable indexes

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ set -ex
 
 yarn run clean
 yarn run build
-rsync -avzH --delete _site/* _site/.htaccess groove2groove:www/
-rsync -rvH --delete --existing --ignore-existing _site/ groove2groove:www/
+rsync -avzH --exclude=/data/checkpoints/ --delete _site/* _site/.htaccess groove2groove:www/
+rsync -rvH --exclude=/data/checkpoints/ --delete --existing --ignore-existing _site/ groove2groove:www/
